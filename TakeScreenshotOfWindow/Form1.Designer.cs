26db91148d0c0555fb7fe1cab508ae4dd48bbe3a
xXx-QuIcks0p3rr2004-Sw4gg3r-xXx <jonathan.lutterbeck@gmail.com>
2019-08-20 23:48:00 +0100

    Initial commit

---
+++ TakeScreenshotOfWindow/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace TakeScreenshotOfWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(54, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "X Position:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(54, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Y Position:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(54, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Width:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(54, 216);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Height:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(209, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(209, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 282);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 305);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ TakeScreenshotOfWindow/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TakeScreenshotOfWindow
+{
+    public partial class Form1 : Form
+    {
+
+        int x, y, w, h;
+        string loc;
+        int xa = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            loc = getLocationofWindow();
+
+            y = StringToIntLocationY(loc);
+            x = StringToIntLocationX(loc);
+            h = StringToIntLocationH(loc);
+            w = StringToIntLocationW(loc);
+            
+        }
+
+
+        public Bitmap Screenshot(int x, int y, int h, int w)
+        {
+            Bitmap bmpScreenshot = new Bitmap(w-14, h-37);
+
+            Graphics g = Graphics.FromImage(bmpScreenshot);
+            
+            string FileLocation = "X:/Programmieren/Daten/EggIncBotScreenshots/" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
+
+            g.CopyFromScreen(y+7,x+30,0,0, new Size(w-14, h-37));
+            bmpScreenshot.Save(FileLocation, ImageFormat.Png);
+            xa++;
+            return bmpScreenshot;
+        }
+
+        public string getLocationofWindow()
+        {
+            int windowHeight = this.Height;
+            int windowWidth = this.Width;
+
+            int windowTop = this.Top;
+            int windowLeft = this.Left;
+
+            string result =  windowTop.ToString() +","+ windowLeft.ToString() + ","+ windowHeight + "," + windowWidth;
+
+            return result;
+        }
+        
+        //converts location string to single intgers x y width and height
+        public int StringToIntLocationX(string LocString)
+        {
+            string LocationOfWindow = getLocationofWindow();
+            string stringx;
+
+            string[] tmp = LocationOfWindow.Split(',');
+            stringx = tmp[0];
+
+            x = Convert.ToInt32(stringx);
+
+            return x;
+        }
+
+
+        private void UpdateLabels()
+        {
+            loc = getLocationofWindow();
+
+            y = StringToIntLocationY(loc);
+            x = StringToIntLocationX(loc);
+            h = StringToIntLocationH(loc);
+            w = StringToIntLocationW(loc);
+
+            label1.Text = x.ToString();
+            label2.Text = y.ToString();
+            label3.Text = w.ToString();
+            label4.Text = h.ToString();
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            UpdateLabels();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            UpdateLabels();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            Screenshot(x, y, h, w);
+        }
+
+        public int StringToIntLocationY(string LocString)
+        {
+            string LocationOfWindow = getLocationofWindow();
+            string stringy;
+
+            string[] tmp = LocationOfWindow.Split(',');
+            stringy = tmp[1];
+            
+            y = Convert.ToInt32(stringy);
+
+            return y;
+        }
+        public int StringToIntLocationW(string LocString)
+        {
+            string LocationOfWindow = getLocationofWindow();
+            string stringw;
+
+            string[] tmp = LocationOfWindow.Split(',');
+            stringw = tmp[3];
+            
+            w = Convert.ToInt32(stringw);
+
+            return w;
+        }
+        public int StringToIntLocationH(string LocString)
+        {
+            string LocationOfWindow = getLocationofWindow();
+            string stringh;
+
+            string[] tmp = LocationOfWindow.Split(',');
+            stringh = tmp[2];
+            
+            h = Convert.ToInt32(stringh);
+
+            return h;
+        }
+    }
+}
